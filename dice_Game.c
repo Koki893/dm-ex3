900c37f453b1b88b4bb8af34f908af8e9860fd58
Koki893 <s1320220@u-aizu.ac.jp>
2025-06-27 12:24:06 +0900

    Greetings added

--- dice_Game.c
+++ dice_Game.c
@@ -2,11 +2,29 @@
 #include <stdlib.h>
 #include <time.h>
 int main(){
+
+  int x1,x2,total;
   char name[55];
+
+  srand(time(NULL));
   
+  x1 = (rand() % 6 + 1);
+  x2 = (rand() % 6 + 1);
+  total = x1 + x2;
+
   printf("What is your name?\n");
   scanf("%s",name);
   printf("> %s\n",name);
   printf("Hello, %s!\n",name);
   
-
+  printf("Rolling dice...\n");
+  printf("Die 1: %d\n",x1);
+  printf("Die 2: %d\n",x2);
+  printf("Total value: %d\n",total);
+  if(total>=8)
+    {
+      printf("You won\n");
+    }
+  else printf("You lost\n");
+  return 0;
+}
