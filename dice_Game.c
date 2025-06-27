a8435aadd0e3daa8d4bc1d1e52baab1529832abc
Koki893 <s1320220@u-aizu.ac.jp>
2025-06-27 12:01:51 +0900

    Greetings added

--- dice_Game.c
+++ dice_Game.c
@@ -1,2 +1,12 @@
-unko
-name
+#include <stdio.h>
+
+int main(){
+
+  char name[55];
+  
+  printf("What is your name?\n");
+  scanf("%s",name);
+  printf("> %s\n",name);
+  printf("Hello, %s!\n",name);
+  return 0;
+}
